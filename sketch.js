d2f79f7b1e93f1f53f2098780a24726ce4f5438b
robotrobo <kanhaagrawal0804@gmail.com>
2018-09-25 01:24:29 +0530

    all commits done for today

--- sketch.js
+++ sketch.js
@@ -3,8 +3,18 @@ let pass = "";
 let lab_user, lab_pass, user_in, pass_in;
 
 function setup() {
+  lab1 = createElement("label", "Enter name:");
+  lab1.position(0, 85)
   user_in = createInput("", "text");
+  user_in.id("username");
+  user_in.attribute("type", "text", );
+  user_in.class("form-control");
+
+  lab2 = createElement("label", "Enter pass:");
   pass_in = createInput("", "password");
+  pass_in.id("pass");
+  pass_in.attribute("type", "password");
+  pass_in.class("form-control");
 
 
 
